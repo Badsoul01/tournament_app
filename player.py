5e3dc5e9012d0277c56907daf2fb711670a6ca37
Badsoul01 <d.jurena@gmail.com>
2026-07-08 15:01:12 +0200

    upravena napověda v metodách

--- player.py
+++ player.py
@@ -17,23 +17,23 @@ class Player:
                 "points": 0
             }
 
-    def write_result(self,tournament_stage,balls_win,balls_lost,game_win,game_lost,points):
+    def write_result(self,tournament_stage:str,balls_win,balls_lost,games_win,games_lost,points):
         if tournament_stage ==  "group_stage":
             self.group_stage["balls_win"] +=balls_win
             self.group_stage["balls_lost"] +=balls_lost
-            self.group_stage["games_win"] += game_win
-            self.group_stage["games_lost"] += game_lost
+            self.group_stage["games_win"] += games_win
+            self.group_stage["games_lost"] += games_lost
             self.group_stage["points"] += points
 
         elif tournament_stage == "playoff_stage":
             self.playoff_stage["balls_win"] += balls_win
             self.playoff_stage["balls_lost"] += balls_lost
-            self.playoff_stage["games_win"] += game_win
-            self.playoff_stage["games_lost"] += game_lost
+            self.playoff_stage["games_win"] += games_win
+            self.playoff_stage["games_lost"] += games_lost
             self.playoff_stage["points"] += points
 
 
-    def difference_of_score(self,tournament_stage):
+    def difference_of_score(self,tournament_stage:str):
         difference_of_balls = 0
         difference_of_games = 0
 
