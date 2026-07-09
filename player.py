3fe40131438feffbb1cbb8904e5cf1aaed39cf3b
Badsoul01 <d.jurena@gmail.com>
2026-07-09 14:02:52 +0200

    předělání metody write_result a přejmenování proměných pro tournament stage

--- player.py
+++ player.py
@@ -2,14 +2,14 @@ class Player:
 
     def __init__(self,name):
         self.name = name
-        self.group_stage = {
+        self.group= {
             "balls_win": 0,
             "balls_lost": 0,
             "games_win": 0,
             "games_lost": 0,
             "points": 0
         }
-        self.playoff_stage = {
+        self.playoff = {
                 "balls_win": 0,
                 "balls_lost": 0,
                 "games_win": 0,
@@ -17,33 +17,33 @@ class Player:
                 "points": 0
             }
 
-    def write_result(self,tournament_stage:str,balls_win,balls_lost,games_win,games_lost,points):
-        if tournament_stage ==  "group_stage":
-            self.group_stage["balls_win"] +=balls_win
-            self.group_stage["balls_lost"] +=balls_lost
-            self.group_stage["games_win"] += games_win
-            self.group_stage["games_lost"] += games_lost
-            self.group_stage["points"] += points
+    def write_result(self,tournament_stage:str,balls_win,balls_lost,games_win,games_lost,points=None):
+        if tournament_stage ==  "Group":
+            self.group["balls_win"] +=balls_win
+            self.group["balls_lost"] +=balls_lost
+            self.group["games_win"] += games_win
+            self.group["games_lost"] += games_lost
+            self.group["points"] += points
 
-        elif tournament_stage == "playoff_stage":
-            self.playoff_stage["balls_win"] += balls_win
-            self.playoff_stage["balls_lost"] += balls_lost
-            self.playoff_stage["games_win"] += games_win
-            self.playoff_stage["games_lost"] += games_lost
-            self.playoff_stage["points"] += points
+        elif tournament_stage == "Playoff":
+            self.playoff["balls_win"] += balls_win
+            self.playoff["balls_lost"] += balls_lost
+            self.playoff["games_win"] += games_win
+            self.playoff["games_lost"] += games_lost
+
 
 
     def difference_of_score(self,tournament_stage:str):
         difference_of_balls = 0
         difference_of_games = 0
 
-        if tournament_stage == "group_stage":
-            difference_of_balls = self.group_stage["balls_win"] - self.group_stage["balls_lost"]
-            difference_of_games = self.group_stage["games_win"] - self.group_stage["games_lost"]
+        if tournament_stage == "Group":
+            difference_of_balls = self.group["balls_win"] - self.group["balls_lost"]
+            difference_of_games = self.group["games_win"] - self.group["games_lost"]
 
-        elif tournament_stage == "playoff_stage":
-            difference_of_balls = self.playoff_stage["balls_win"] - self.playoff_stage["balls_lost"]
-            difference_of_games = self.playoff_stage["games_win"] - self.playoff_stage["games_lost"]
+        elif tournament_stage == "Playoff":
+            difference_of_balls = self.playoff["balls_win"] - self.playoff["balls_lost"]
+            difference_of_games = self.playoff["games_win"] - self.playoff["games_lost"]
 
         return difference_of_balls,difference_of_games
 
