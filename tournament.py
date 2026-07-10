b94d4e5ceabd3f54d43218e5546f24fe99291e94
Badsoul01 <d.jurena@gmail.com>
2026-07-10 23:16:45 +0200

    uprava inicializace a propojení s třídou pro nastavení turnaje

--- tournament.py
+++ tournament.py
@@ -1,22 +1,24 @@
+from tournamentconfig import TournamentConfig
 from player import Player
 from group import Group
 from playoff import Playoff
 
+
 class Tournament:
 
-    def __init__(self,name:str,groups_data_from_web:dict,settings_from_web:dict):
+    def __init__(self,name:str,groups_data_from_web:dict,config: TournamentConfig):
         self.name = name
         self.raw_groups_data = groups_data_from_web
 
         #---- Pravidla ----
 
         #Formáty zápasů
-        self.group_match_format = settings_from_web.get("group_match_format","2 sets")
-        self.playoff_match_format = settings_from_web.get("playoff_match_format","Best of 3")
+        self.group_match_format = config.group_match_format
+        self.playoff_match_format = config.playoff_match_format
         #pravidla pro postup a vyřazení
-        self.advancing_per_group = settings_from_web.get("advancing_per_group",2)
-        self.non_advancing_action = settings_from_web.get("non_advancing_action","KO")
-        self.playoff_losers_action = settings_from_web.get("playoff_losers_action", "KO")
+        self.advancing_per_group = config.advancing_per_group
+        self.non_advancing_action = config.non_advancing_action
+        self.playoff_losers_action = config.playoff_losers_action
 
         # ---- Objekty ----
 
