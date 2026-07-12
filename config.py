3bad2a5f6df41f9f716bc1fa101597097850f8c6
Badsoul01 <d.jurena@gmail.com>
2026-07-12 23:14:15 +0200

    uprava pravidel pro turnaj a přidání možností

--- config.py
+++ config.py
@@ -1,31 +1,42 @@
-RULES_OPTIONS = {
-    #počet skupin
-    "total_groups_count":[1,2,3,4,5,6,7,8],
+TOURNAMENT_RULES= {
+    "tournament_format": {
+        "groups_and_playoff": "skupiny a playoff",
+        "group": "skupina",
+        "playoff": "playoff"
+    },
+}
 
-    #formát zápasů
-    "group_match_format": {"2": "2 sets",
-                     "BO3": "Best of 3",
-                     "BO5": "Best of 5"
-                    },
-    "playoff_match_format":{
-        "BO3": "Best of 3",
-        "BO5": "Best of 5"
+GROUPS_RULES = {
+    "group_match_format": {
+        2: "2 hrané sety",
+        3: "2 vítězné sety",
+        5: "3 vítězné sety"
         },
-
-    #pravidla pro postup a vyřazování
+    "min_players_per_group": 1,
+    "max_players_per_group": 10,
+    "min_group": 1,
+    "max_group": 1 if TOURNAMENT_RULES["tournament_format"]== "group" else  26,
+    "group_creation_options":[1,2,4,6],
+    "min_advance_per_group": 1,
+    "max_advance_per_group": 4,
     "advance_per_group":[1,2,3,4],
-    "players_per_group_limit":[4,5,6],
-
-
-
-    "non_advancing_action": {
+    "elimination_actions": {
         "playoff_b": "Postup do pavouka B",
         "minigroup": "Mini-skupina o pořadí",
         "KO": "Konec v turnaji"
     },
-    "playoff_losers_action":{
-        "consolation":"Turnaj útěchy",
-        "KO": "Konec v turnaji"
 
-    }
+}
+
+PLAYOFF_RULES = {
+    "playoff_match_format":{
+        3: "2 vítězné sety",
+        5: "3 vítězné sety",
+        7: "4 vítězné sety"
+                    },
+    "elimination_actions": {
+        "consolation":"Dohrávka o pořadí",
+        "KO": "Konec v turnaji"
+        }
+
 }
