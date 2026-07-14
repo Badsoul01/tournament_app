01727fd25740f69f4d1cb16a4cc364cd9da5f9da
Badsoul01 <d.jurena@gmail.com>
2026-07-14 21:09:14 +0200

    smazání počítání bodů pro playoff

--- player.py
+++ player.py
@@ -14,7 +14,6 @@ class Player:
                 "balls_lost": 0,
                 "games_win": 0,
                 "games_lost": 0,
-                "points": 0
             }
 
     def write_result(self,tournament_stage:str,balls_win,balls_lost,games_win,games_lost,points=None):
