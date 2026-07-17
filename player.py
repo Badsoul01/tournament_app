1f0dd9786e052fe6df04306861a479115bcc18c7
Badsoul01 <d.jurena@gmail.com>
2026-07-17 09:16:27 +0200

    přidána metoda na třízení hráče a opraveny chyby

--- player.py
+++ player.py
@@ -2,50 +2,46 @@ class Player:
 
     def __init__(self,name):
         self.name = name
-        self.group= {
-            "balls_win": 0,
-            "balls_lost": 0,
-            "games_win": 0,
-            "games_lost": 0,
-            "points": 0
-        }
-        self.playoff = {
+        self.stats= {}
+
+    def _ensure_stage(self,stage_name):
+        if stage_name not in self.stats:
+            self.stats[stage_name] = {
                 "balls_win": 0,
                 "balls_lost": 0,
                 "games_win": 0,
                 "games_lost": 0,
+                "points": 0
             }
-
-    def write_result(self,tournament_stage:str,balls_win,balls_lost,games_win,games_lost,points=None):
-        if tournament_stage ==  "Group":
-            self.group["balls_win"] +=balls_win
-            self.group["balls_lost"] +=balls_lost
-            self.group["games_win"] += games_win
-            self.group["games_lost"] += games_lost
-            self.group["points"] += points
-
-        elif tournament_stage == "Playoff":
-            self.playoff["balls_win"] += balls_win
-            self.playoff["balls_lost"] += balls_lost
-            self.playoff["games_win"] += games_win
-            self.playoff["games_lost"] += games_lost
+    def write_result(self,tournament_stage,balls_win,balls_lost,games_win,games_lost,points=0):
+        self._ensure_stage(tournament_stage)
+        s = self.stats[tournament_stage]
+        s["balls_win"] +=balls_win
+        s["balls_lost"] +=balls_lost
+        s["games_win"] += games_win
+        s["games_lost"] += games_lost
+        s["points"] += points
 
 
+    def difference_of_score(self,tournament_stage):
+        if tournament_stage not in self.stats:
+            return {"Balls":0, "Games":0}
 
-    def difference_of_score(self,tournament_stage:str):
-        difference_of_balls = 0
-        difference_of_games = 0
-
-        if tournament_stage == "Group":
-            difference_of_balls = self.group["balls_win"] - self.group["balls_lost"]
-            difference_of_games = self.group["games_win"] - self.group["games_lost"]
-
-        elif tournament_stage == "Playoff":
-            difference_of_balls = self.playoff["balls_win"] - self.playoff["balls_lost"]
-            difference_of_games = self.playoff["games_win"] - self.playoff["games_lost"]
+        s = self.stats[tournament_stage]
 
         return {
-            "Balls":difference_of_balls,
-            "Games":difference_of_games
+            "Balls":s["balls_win"]-s["balls_lost"],
+            "Games":s["games_win"]- s["games_lost"]
         }
 
+    def get_sorting_stats(self,stage_name):
+        if stage_name not in self.stats:
+            return (0,0,0)
+        s = self.stats[stage_name]
+        diff = self.difference_of_score(stage_name)
+
+        return (
+            s.get("points",0),
+            diff["Games"],
+            diff["Balls"]
+        )
