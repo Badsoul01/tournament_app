69c77f0db6d840554d95b64ffca05efcdefec97c
Badsoul01 <d.jurena@gmail.com>
2026-07-17 16:24:23 +0200

    opravy překlepů

--- match.py
+++ match.py
@@ -2,20 +2,16 @@ from player import Player
 
 class Match:
 
-    def __init__(self, player_a:Player,player_b:Player,match_format:str, tournament_stage:str,match_id=None):
+    def __init__(self, player_a:Player,player_b:Player,match_format:str, tournament_stage:str,match_id):
         self.player_A = player_a
         self.player_B = player_b
         self.match_format = match_format
         self.tournament_stage = tournament_stage
         self.match_id = match_id
         self.winner = None
-        self.losser = None
+        self.loser = None
         self.is_finished = False
         self.played_sets=[]
-
-    def get_winner(self):
-        return self.winner
-
 
     def evaluate_match(self,played_sets:list[tuple]):
         self.played_sets = played_sets
@@ -49,8 +45,8 @@ class Match:
 
         if player_a["games_win"]>player_b["games_win"]:
             player_a["points"] +=3
-            self.winner = player_a
-            self.losser = player_b
+            self.winner = self.player_A
+            self.loser = self.player_B
 
         elif player_a["games_win"] == player_b["games_win"]:
             player_a["points"] +=1
@@ -58,8 +54,8 @@ class Match:
 
         else:
             player_b["points"] +=3
-            self.winner = player_b
-            self.losser = player_a
+            self.winner = self.player_B
+            self.loser = self.player_A
 
         self.player_A.write_result(tournament_stage=self.tournament_stage,
                                    balls_win=player_a["balls_win"],
