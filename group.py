1ac0a61c0ff41b1f6fa5ce11c6d766136ab0626c
Badsoul01 <d.jurena@gmail.com>
2026-07-17 16:23:06 +0200

    opravy metod

--- group.py
+++ group.py
@@ -1,5 +1,6 @@
 import random
 import itertools
+
 from player import Player
 from match import Match
 
@@ -13,8 +14,8 @@ class Group:
         self.group_matches = {key:[] for key in self.groups.keys()}
 
 
-    def generate_matches(self):
-        match_counter = 1
+    def generate_matches(self,tournament):
+
         for group_name, players in self.groups.items():
             matches = []
             for player_a, player_b in itertools.combinations(players,2):
@@ -22,9 +23,8 @@ class Group:
                                      player_b=player_b,
                                      match_format=self.match_format,
                                      tournament_stage=self.stage_name,
-                                     match_id=match_counter
+                                     match_id=tournament.get_next_match_id()
                                      ))
-                match_counter+=1
 
             random.shuffle(matches)
             self.group_matches[group_name]= matches
@@ -42,6 +42,19 @@ class Group:
 
         return sorted_players
 
+    @staticmethod
+    def apply_snake_seeding(players):
+        snake_ordered = []
+        temp_list = list(players)
+        left = True
+        while temp_list:
+            if left:
+                snake_ordered.append(temp_list.pop(0))
+            else:
+                snake_ordered.append(temp_list.pop(-1))
+            left = not left
+
+        return snake_ordered
 
     def are_all_matches_played(self,group_name):
         """Vratí True,pokud jsou všechny zápasy ve skupině dohrané."""
