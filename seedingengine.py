b21e808771811179d57caf4cd8a9528210b28559
Badsoul01 <d.jurena@gmail.com>
2026-07-30 23:53:06 +0200

    upravení metd generate_grouped_bracket

--- seedingengine.py
+++ seedingengine.py
@@ -7,15 +7,17 @@ class SeedingEngine:
     # VEŘEJNÉ API (HLAVNÍ METODY)
     # ==========================================
 
-    def build_first_round(self,groups: dict, players: list = None, advance_per_group: int) -> list:
+    def build_first_round(self,groups: dict, players: list = None, advance_per_group: int=0) -> list:
         """
         Hlavní metoda, která funguje jako vyhybka.
         Podle formátu turnaje rozhodne, jaký matematický algoritmus se použije.
         """
         if self.tournament_format == "groups_and_playoff":
+            print(F"DEBUG: POUŽIT GROUPED METODA {self.tournament_format}")
             return self._generate_grouped_bracket(groups, advance_per_group)
 
         elif self.tournament_format == "playoff":
+            print(F"DEBUG: POUŽIT ATP METODA {self.tournament_format}")
             return self._generate_atp_bracket(players=players)
 
         return []
@@ -30,11 +32,8 @@ class SeedingEngine:
         Vygeneruje nasazení pole žebříčku/pořadí v seznamu.
         První hraje s posledním atd.
         """
-        # 1. Zjistíme velikost pavouka (nejbližší mocnina dvou)
-        bracket_size = 2
-
-        while bracket_size < len(players):
-            bracket_size *= 2
+        if not players:
+            return []
 
         # Zjistíme počet potřebných BYE
         byes_needed = self._calculating_byes(len(players))
@@ -63,22 +62,26 @@ class SeedingEngine:
         Vrátí pořadí seedů pro standardní vyřazovací pavouk.
         Např. pro 8 hráčů: 1-8,4-5,3-6,2-7
         """
-
         if size <=1:
             return [0]
 
-        bracket = [0,1]
-        while len(bracket) < size:
-            next_bracket = []
-            length = len(bracket) * 2
+        brackets = [0]
+        while len(brackets) < size:
+            next_brackets = []
 
-            for seed in bracket:
-                next_bracket.append(seed)
-                next_bracket.append(length - 1- seed)
+            for i, idx in enumerate(brackets):
+                pair = (idx, 2 * len(brackets) -1 - idx)
 
-            bracket = next_bracket
+                # Univerzální zrcadlo:
 
-        return bracket
+                if i % 2 == 1:
+                    pair = (pair[1],pair[0])
+
+                next_brackets.extend(pair)
+
+            brackets = next_brackets
+            print(f"DEBUG: počet bracketů: {brackets}")
+        return brackets
 
     def _calculating_byes(self, total_slots: int) -> int:
         """
@@ -93,4 +96,42 @@ class SeedingEngine:
         """
         Připrava pro nový algoritmus
         """
-        pass
+        # 1. Nasbíráme všechna postupová místa ze všech skupin
+        group_names = sorted(groups.keys())
+        num_bracket = len(group_names)
+
+        # 1. Vytvoříme prázdné koše (čtvrtiny/poloviny) podle počtu skupin
+        buckets = [[] for _ in range(num_bracket)]
+
+        # 2. Do každého koše jedno písmenko, rotujeme pro každé pořadí
+        for rank in range(1, advance_per_group + 1):
+            # Posuneme abecedu o 0,1,2.... pozice
+            shift = rank-1
+            rotated_groups = group_names[shift:] + group_names[:shift]
+
+            #Rozdáme hráče do košů
+            for i in range(num_bracket):
+                buckets[i].append(f"{rank}{rotated_groups[i]}")
+
+        # Každý koš samostatně domplníme o Bye a necháme ATP, at ho spáruje unvitř
+        final_matches = []
+        bucket_order = self._get_seeding_indices(num_bracket)
+
+        for target_position, bucket_index in enumerate(bucket_order):
+            bucket = buckets[bucket_index]
+
+            byes_needed = self._calculating_byes(len(bucket))
+            for _ in range(byes_needed):
+                bucket.append(None)
+
+            # ATP nám spáruje prvního s posledním unvitř čtvrtiny
+            bucket_matches = self._generate_atp_bracket(bucket)
+
+            # Pokud jsme ve "spodní části jakékoliv větve (lichý index
+            # zápasy otočíme, aby favorit padl na vnější okraj pavouka.
+            if target_position % 2 == 1:
+                bucket_matches.reverse()
+
+            final_matches.extend(bucket_matches)
+
+        return final_matches
