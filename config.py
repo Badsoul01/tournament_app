51bbd91d9b7bc758ab0267f502b347c8b3e35a36
Badsoul01 <d.jurena@gmail.com>
2026-08-05 10:34:28 +0200

    přidáno max byes a počet hráčů pro možný postup a kontrolu, dále nastavene brackety (8,16,32)

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ GROUPS_RULES = {
         5: "3 vítězné sety"
         },
     "min_players_per_group": 3,
+
     "max_players_per_group": 8,
     "min_group": 1,
     "max_group": 1 if TOURNAMENT_RULES["available_formats"]== "group" else  8,
@@ -31,6 +32,8 @@ GROUPS_RULES = {
 }
 
 PLAYOFF_RULES = {
+    "max_byes_count" : 4,
+    "players_allowed_to_playoff" : [6,7,8,12,14,15,16,28,32],
     "playoff_match_format":{
         3: "2 vítězné sety",
         5: "3 vítězné sety",
@@ -40,5 +43,28 @@ PLAYOFF_RULES = {
         "consolation":"Dohrávka o pořadí",
         "KO": "Konec v turnaji"
         }
+
+}
+# PLAYOFF STRUTRURA
+
+starter_edition = {
+    8 : [0,1,2,3],
+    "8_reverse" : [3,2,1,0],
+    8_1 : ["BYE_idx",1,2,3],
+    "8_1_reverse" : [3,2,1,"BYE_idx"],
+    8_2 : ["BYE_idx",1,2,"BYE_idx"],
+    "8_2_reverse": ["BYE_idx",2,1,"BYE_idx"]
+}
+
+classic_edition = {
+    16 : starter_edition[8]+ starter_edition["8_reverse"],
+    16_1: starter_edition["8_1_reverse"] + starter_edition["8_reverse"],
+    16_2: starter_edition[8_1] + starter_edition["8_1_reverse"],
+    16_4: starter_edition[8_2] + starter_edition["8_2_reverse"]
+}
+
+moderate_edition = {
+    32 : classic_edition[16] *2 *2,
+    32_4: classic_edition[16_2]*2*2
 
 }
