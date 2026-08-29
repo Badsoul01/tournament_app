5c89bb0986ee3235fe0b8462612f04815a468ab3
Badsoul01 <d.jurena@gmail.com>
2026-08-29 14:18:51 +0200

    uprava souboru

--- models.py
+++ models.py
@@ -9,6 +9,8 @@ class Tournament(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     date = db.Column(db.Date, default=lambda: datetime.now().date())
+    group_match_format = db.Column(db.Integer)
+    playoff_match_format= db.Column(db.Integer)
     advance_per_group = db.Column(db.Integer)  # Stačí nám číslo
     group_elimination_action = db.Column(db.String(50))
     playoff_elimination_action = db.Column(db.String(50))
